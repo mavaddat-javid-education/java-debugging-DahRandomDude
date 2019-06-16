53ab0681b3d92292a51eee5021d632ac742c2293
DahRandomDude <jason0519@JasondeMBP.hitronhub.home>
2019-06-15 18:12:20 -0700

    something

--- Chapter02/DebuggingExercises/DebugTwo1.java
+++ Chapter02/DebuggingExercises/DebugTwo1.java
@@ -1,15 +1,15 @@
 public class DebugTwo1
 {
-   public static void main(String[] args)
-   {
-      int oneInt = 315.0;
-      double oneDouble = 12.4;
-      char oneChar = A;
-      System.out.print("The int is ");
-      System.out.pintln(oneInt);
-      System.out.print("The double is ");
-      System.println(oneDouble);
-      System.out.print(The char is ");
-      System.out.println(oneChar);
-   }
+  public static void main(String[] args)
+    {
+       int oneInt = 315;
+       double oneDouble = 12.4;
+       char oneChar = 'A';
+       System.out.print("The int is ");
+       System.out.println(oneInt);
+       System.out.print("The double is ");
+       System.out.println(oneDouble);
+       System.out.print("The char is ");
+       System.out.println(oneChar);
+    }
 }
