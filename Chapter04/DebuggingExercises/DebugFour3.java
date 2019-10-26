2e8aa476ddd71ab9d56e5afeba3723e10b6e867a
DahRandomDude <51520831+DahRandomDude@users.noreply.github.com>
2019-10-26 15:59:15 -0700

    Finished DBFour3 & Box

--- Chapter04/DebuggingExercises/DebugFour3.java
+++ Chapter04/DebuggingExercises/DebugFour3.java
@@ -3,23 +3,23 @@ public class DebugFour3
 {
    public static void main(String args[])
    {
-      int width = 12,
-      length = 10,
-      height = 8;
-      FixDebugBox box1 = new DebugBox();
-      FixDebugBox box2 = new DebugBox(width, length, height);
+      int width = 12;
+      int length = 10;
+      int height = 8;
+      DebugBox box1 = new DebugBox();
+      DebugBox box2 = new DebugBox(width, length, height);
       System.out.println("The dimensions of the first box are");
-      showData();
+      box1.showData();
       System.out.print("  The volume of the first box is ");
-      box1.showVolume();
+      showVolume(box1);
       System.out.println("The dimensions of the second box are");
       box2.showData();
       System.out.print("  The volume of the second box is ");
-      box2.showVolume();
+      showVolume(box2);
    }
    public static void showVolume(DebugBox aBox)
    {
       double vol = aBox.getVolume();
       System.out.println(vol);
    }
-
+}
