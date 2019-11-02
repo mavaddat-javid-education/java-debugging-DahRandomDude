a98c34320b8d8a4442c0036e402b58d49906ad73
DahRandomDude <51520831+DahRandomDude@users.noreply.github.com>
2019-11-02 15:34:58 -0700

    Done Chap4

--- Chapter04/DebuggingExercises/DebugFour4.java
+++ Chapter04/DebuggingExercises/DebugFour4.java
@@ -3,22 +3,22 @@ public class DebugFour4
 {
    public static void main(String args[])
    {
-      final double DISCOUNT_RATE = 0.90;
+      final double discountRate = 0.90;
       int price = 100;
       double price2 = 100.00;
-      tenPercentOff(price, DISCOUNT_RATE);
-      tenPercentOff(price2, DISCOUNT_RATE);
+      tenPercentOff(price, discountRate);
+      tenPercentOff(price2, discountRate);
    }
-   public static void tenPercentOff(int p)
+   public static void tenPercentOff(int p, double dR)
    {
-      double newPrice = p * DISCOUNT_RATE;
-      System.out.println("Ten percent off  + p);
+      double newPrice = p * dR;
+      System.out.println("Ten percent off "  + p);
       System.out.println("  New price is " + newPrice);
    }
-   public static void tenPercentOff(double p)
+   public static void tenPercentOff(double p2, double dR)
    {
-      double newPrice = DISCOUNT_RATE;
-      System.out.println("Ten percent off " + p);
+      double newPrice = dR*p2;
+      System.out.println("Ten percent off " + p2);
       System.out.println("  New price is " + newPrice);
    }
 }
